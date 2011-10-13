6f68def62988584b835000b8196478de91c17b06
DeepRootNine <drnine@unreal-world.de>
2011-10-14 01:10:12 +0200

    Added Project and sourcefiles

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2011-10-03
+ *      Author: stefan
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World!\n";
+	return 0;
+}
